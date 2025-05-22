b6585ff224a6829e00966fd0b583925d1887d111
Taofeek <ibrahimtaofeek005@gmail.com>
2025-05-22 18:38:29 +0100

    web token added to login route

--- utilis/hash.js
+++ utilis/hash.js
@@ -5,10 +5,10 @@ exports.hashPassword = (value, saltValue) => {
     return result;
 }
 
-exports.hashPasswordValidation = (value, hashedValue) => {
-    const result = compare(value, hashedValue);
-    return result;
-}
+// exports.hashPasswordValidation = (value, hashedValue) => {
+//     const result = compare(value, hashedValue);
+//     return result;
+// }
 
 // exports.hmacProcess = (value, key) => {
 //     const result = createHmac('sha256', key).update(value).digest('hex');
