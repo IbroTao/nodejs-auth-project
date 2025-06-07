1788b270e605937d077a5ed3883f77c25c2f9c1a
Taofeek <ibrahimtaofeek005@gmail.com>
2025-06-07 14:54:23 +0100

    identifier added in middleware

--- middlewares/authorize.js
+++ middlewares/authorize.js
@@ -9,6 +9,20 @@ exports.identifier = (req, res, next) => {
     }
 
     if(!token) {
-        return res.status(403).json({success: false})
+        return res.status(403).json({success: false, message: "Unauthorized!"})
+    };
+
+
+    try {
+        const userToken = token.split(' ')[1]
+        const jwtVerified = jwt.verify(userToken, process.env.JWT_SECRET);
+        if(jwtVerified) {
+            req.user = jwtVerified;
+            next();
+        } else {
+            throw new Error("token error")
+        }
+    } catch (error) {
+        
     }
 }
